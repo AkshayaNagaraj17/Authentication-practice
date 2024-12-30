aa49a3e0703a92545cbf85fa04b0595a03892c9a
AkshayaNagaraj <cnagaraj332@gmail.com>
2024-12-30 21:00:29 +0530

    Tokens practised

--- token.js
+++ token.js
@@ -5,7 +5,16 @@ app.use(express.json())
 //with stores the users and tokens as well
 const users=[]
 
-
+function generateToken()
+{
+    let token=''
+    let options=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','z','0','1','2','3','4','5','6','7','8','9']
+    for(let i=0;i<32;i++)
+    {
+        token = token + options[Math.floor(Math.random() * options.length)];
+    }
+    return token
+}
 app.post("/signup",function(req,res)
 {
     const uname=req.body.uname
@@ -13,11 +22,11 @@ app.post("/signup",function(req,res)
     //pushing the new users in users array
     users.push(
         {
-            username:uname,
-            password:pword
+            uname:uname,
+            pword:pword
         }
     )
-    req.json(
+    res.json(
         {
             message:"You are signed up"
         }
@@ -27,7 +36,10 @@ app.post("/signup",function(req,res)
 
 
 app.post("/sign-in",function(req,res)
+
 {
+    const uname = req.body.uname;
+    const pword = req.body.pword;
     let foundUser=null
     for(let i=0 ; i<users.length;i++)
     {
@@ -53,5 +65,27 @@ app.post("/sign-in",function(req,res)
                 message:"Invalid user"
             }
         }
+        console.log(users)
 })
+
+
+
+app.get("/me",function(req,res)
+{
+    const token=req.headers.token
+    const user=users.find(user=>user.token===token)
+    if(user)
+    {
+        res.json({
+            yourName:user.uname
+        })
+    }
+    else{
+        res.status(401).send({
+            message:"Unauthorized"
+        })
+    }
+})
+
+
 app.listen(3005)
